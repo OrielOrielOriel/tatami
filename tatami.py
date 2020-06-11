511f101e89a700309bf9d7eb652b733ed5543121
Oriel <orianafarrugia3@gmail.com>
2020-06-10 20:19:31 -0500

    Updated comments/options and added current_time

--- tatami.py
+++ tatami.py
@@ -11,7 +11,18 @@ import pandas
 import time
 
 BANNER = """
-Sup
+
+
+  ████████╗ █████╗ ████████╗ █████╗ ███╗   ███╗██╗
+  ╚══██╔══╝██╔══██╗╚══██╔══╝██╔══██╗████╗ ████║██║
+     ██║   ███████║   ██║   ███████║██╔████╔██║██║
+     ██║   ██╔══██║   ██║   ██╔══██║██║╚██╔╝██║██║
+     ██║   ██║  ██║   ██║   ██║  ██║██║ ╚═╝ ██║██║
+     ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝
+                                                
+Developed by Oriel & Spopy
+github.com/OrielOrielOriel
+github.com/sq00ky
 """
 
 #  Defines path as the parent directory of the script + the character '/'
@@ -43,35 +54,66 @@ def parseArguments():
 		type=str,
 		action='extend',
 		nargs='+',
-		help='Data to be retrieved.'
+		help='Data to be retrieved. Common fields ip_str=Host IP address, hostnames=Host name, port=Open ports, product=Product version on the particular port, banner=Banner from a connection to the port.'
 	)
 
 	return parser
 
+"""
+Returns a list of targets from a file where
+each line is considered a separate target. 
 
+Lines which are blank or start with a '#'
+are ignored.
+""" 
 def loadTargets(parserMetavar):
 	if parserMetavar is None: return []
 	with open(parserMetavar, "r") as all_targets:
 		return [target.rstrip().split() for target in all_targets.readlines() if target.strip() and not target.startswith("#")]
 
 
+"""
+Iterates over a list of targets and performs
+a get request to Shodan's /host/{ip} endpoint
+where {ip} is the target.
+
+Uses the API key provided by -a / --api-key.
+
+Yields the text reponse received from the get
+request.
+"""   
 def getHostInfo(api_key, targets):
 	for target in targets:
 		yield requests.get(f'https://api.shodan.io/shodan/host/{target[0]}?key={api_key}').text
 
 
-def yieldData(dataset, options):
+"""
+Accepts a dictionary via 'dataset' and a 
+list via 'options'
+
+Each object in the list will be used as a key
+for the dictionary 'dataset.' 
+
+Yields the value 
+associated with the key if that key is present
+and has content associated with it, else it 
+yields "N/A"
+"""
+def yieldData(dataset: dict, options: list):
 	for option in options:
 		try:
-			if option != "hostnames":
-				yield str(dataset[option])
-
-			else:
+			if option == "hostnames":
 				if not dataset[option]:
 					yield "N/A"
 
 				else:
 					yield str(dataset[option][0])
+
+			if option == "current_time":
+				yield str(current_time)
+
+			else:
+				yield str(dataset[option])
 
 		except KeyError:
 			yield "N/A"
@@ -95,12 +137,18 @@ def main():
 	results = [result for result in getHostInfo(parser.api_key, targets)]
 	# current_time = time.strftime(r"%m/%d/%Y")
 
+	# Iterates over each API result
 	for result in results:
-		jsonobj = json.loads(result)
+		jsonobj = json.loads(result)  # Loads the result as a json object.
 
 		try:
+
+			# Iterates over each dictionary in the 
+			# primary "data" field. 
 			for dataset in jsonobj["data"]:
+				# Joins each object in the list with a "\t" (tab).
 				row = "\t".join(
+					# Creates a list of the string objects yielded by yieldData()
 					[datapoint for datapoint in yieldData(dataset, parser.options)]
 				)
 
